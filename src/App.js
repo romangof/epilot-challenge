0766b738a1163b67987262a064f0347124e801ab
Roman <roman.fuentes@fleetster.net>
2019-12-04 11:01:39 +0100

    adds material-ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { CssBaseline } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
 import './App.css';
+
+const outerTheme = createMuiTheme({
+    palette: {
+        type: 'dark',
+        primary: { main: '#FAAB1A' },
+        secondary: { main: '#ffffff' }
+    },
+    typography: {
+        fontFamily: ['Roboto', 'Arial', 'sans-serif'].join(',')
+    }
+});
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                Init
-            </header>
-        </div>
+        <ThemeProvider theme={outerTheme}>
+            <div className="App">
+                <header className="App-header">
+                    Init
+                </header>
+            </div>
+
+            <CssBaseline />
+        </ThemeProvider>
     );
 }
 
